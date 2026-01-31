0b415c8243cb973cbffe80082f474c865388d649
suouaki <momobako@myyahoo.com>
2026-01-31 09:48:45 +0800

    Update i18n.ts

--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -6,6 +6,10 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import jp from '../i18n/locales/jp.json';
 import zhCN from '../i18n/locales/zh-CN.json';
 import en from '../i18n/locales/en.json';
+import kr from '../i18n/locales/kr.json';
+import ru from '../i18n/locales/ru.json';
+import Fa from '../i18n/locales/Fa.json';
+import zhTW from '../i18n/locales/zh-TW.json';
 
 
 // 配置i18next
@@ -19,6 +23,18 @@ i18n
     resources: {
       'jp': {
         translation: jp,
+      },
+      'zh-TW': {
+        translation: zhTW,
+      },
+      'Fa': {
+        translation: Fa,
+      },
+      'ru': {
+        translation: ru,
+      },
+      'kr': {
+        translation: kr,
       },
       'zh-CN': {
         translation: zhCN,
